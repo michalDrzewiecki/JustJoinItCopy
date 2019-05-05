53eaa1ff168716cd0d09be1d02d4495af693ea59
Michał Drzewiecki <drzewiecki.mt@gmail.com>
2019-05-05 16:48:53 +0200

    routing dla responsywnosci, wlaczona mapa

--- src/app/shared/components/filter/filter-content-sm/filter-content-sm.component.ts
+++ src/app/shared/components/filter/filter-content-sm/filter-content-sm.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { NavigationService } from 'src/app/shared/services';
+import { Subscription } from 'rxjs';
+import { Router, Event, NavigationEnd } from '@angular/router';
+import { MyParams } from 'src/app/shared/shared.enums';
 
 @Component({
   selector: 'app-filter-content-sm',
@@ -11,11 +14,52 @@ export class FilterContentSmComponent implements OnInit {
   hiddenCities: string[] = [];
   mainTechnologies: string[] = [];
   hiddenTechnologies: string[] = [];
+  subscription: Subscription;
+  @ViewChild('cities') cities: ElementRef; 
+  @ViewChild('technologies') technologies: ElementRef; 
   
-  constructor(private navigationService: NavigationService){}
+  constructor(private navigationService: NavigationService, private router: Router){}
 
   ngOnInit(){
     this.navigationService.getCities(this.mainCities, this.hiddenCities);
     this.navigationService.getTechnologies(this.mainTechnologies, this.hiddenTechnologies);
+    this.subscription = this.router.events.subscribe((event: Event) => {
+      if(event instanceof NavigationEnd ){
+        this.setInitialCity(this.navigationService.checkParam(this.router, MyParams.city))
+        this.setInitialTechnology(this.navigationService.checkParam(this.router, MyParams.technology))
+      }
+    });
+  }
+
+  setInitialCity(value: string): void{
+    if(value != null){
+      for(let city of this.mainCities.concat(this.hiddenCities)){
+        if(value == city){
+          this.cities.nativeElement.value = value;
+          return;
+        }
+      }
+    }
+  }
+
+  setInitialTechnology(value: string): void{
+    if(value != null){
+      for(let technology of this.mainTechnologies.concat(this.hiddenTechnologies)){
+        if(value == technology){
+          this.technologies.nativeElement.value = value;
+          return;
+        }
+      }
+    }
+  }
+
+  cityChange():void{
+    let value: string = this.cities.nativeElement.value;
+    this.navigationService.setParam(this.router, MyParams.city, value);
+  }
+
+  technologyChange():void{
+    let value: string = this.technologies.nativeElement.value;
+    this.navigationService.setParam(this.router, MyParams.technology, value);
   }
 }
